899f4557e003b5e21082a643423b4fe02a0f114e
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-08 00:17:22 +0100

    added bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const buttonEl = document.querySelector("button");
+let newElement;
 
 buttonEl.addEventListener("click",
  function() {
     
 // Creazione griglia da 10 x 10
-    const gridElement = document.querySelector(".grid");
+    let gridElement = document.querySelector(".grid");
+    gridElement.innerHTML = "";
+    let gridLevel = decideDifficult();
 
-    for(let i = 1; i <= 100; i++){
+    for(let i = 1; i <= gridLevel; i++){
 
-        const newElement = document.createElement("div");
-        newElement.className = "square";
+        newElement = document.createElement("div");
         newElement.innerHTML = i;
         newElement.style.fontWeight = "bold";
         newElement.style.fontSize = "14px";
+        otherDifficultClass(newElement);
 
         newElement.addEventListener("click", 
         function(){
@@ -26,3 +29,48 @@ buttonEl.addEventListener("click",
     }
 
  })
+
+ function decideDifficult() {
+    let difficultOptions = 0;
+    let selectOptions = document.getElementById(`options`).value;
+
+    if(selectOptions == "1") {
+
+        difficultOptions = 49;
+    }
+
+    if(selectOptions == "2") {
+
+        difficultOptions = 81;
+    }
+
+    if(selectOptions == "3") {
+
+        difficultOptions = 100;
+    }
+
+    return difficultOptions;
+ }
+
+
+ function otherDifficultClass(sizeCell) {
+
+    let squareClass = "a";
+
+    if(decideDifficult() == 49){
+        squareClass = "little-square";
+        sizeCell.classList.add(squareClass);
+
+    }
+    
+    if(decideDifficult() == 81) {
+        squareClass = "medium-square";
+        sizeCell.classList.add(squareClass);
+    }
+    
+    if(decideDifficult() == 100){
+        squareClass = "square";
+        sizeCell.classList.add(squareClass);
+    }
+
+ }
